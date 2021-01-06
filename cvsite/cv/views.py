9f027ec9a442d254d9fa3574749bd071c87d0b46
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-05 19:32:15 -0800

    Handle POST form request and save to model

--- cvsite/cv/views.py
+++ cvsite/cv/views.py
@@ -3,4 +3,28 @@ from .models import Profile
 
 
 def accept(request):
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        email = request.POST.get("email", "")
+        phone = request.POST.get("phone", "")
+        summary = request.POST.get("summary", "")
+        degree = request.POST.get("degree", "")
+        school = request.POST.get("school", "")
+        university = request.POST.get("university", "")
+        previous_work = request.POST.get("previous_work", "")
+        skills = request.POST.get("skills", "")
+
+        profile = Profile(
+            name=name,
+            email=email,
+            phone=phone,
+            summary=summary,
+            degree=degree,
+            school=school,
+            university=university,
+            previous_work=previous_work,
+            skills=skills,
+        )
+        profile.save()
+
     return render(request, "cv/accept.html")
